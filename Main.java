71709f54072359c6c1cf4f8e2cf70914ed1da534
Craig Thomas <craig.d.thomas.2016@uni.strath.ac.uk>
2019-11-29 16:20:19 +0000

    Game of Life Finished

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] arg) {
-
-        //Create the Game of Life in a 10x10 Board
-        new GameofLifeGUI(10,10);
-    }
-
-}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main {
+
+    public static void main(String[] arg) {
+
+        //Create the Game of Life in a 10x10 Board
+        new GameofLifeGUI(10,10);
+    }
+
+}
